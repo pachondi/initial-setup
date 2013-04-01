ff28df3637c35345e5cbb9531d6a307d2a6e9767
Riaz Gaffoor <silverriaz@gmail.com>
2013-03-31 23:28:17 -0400

    Initial Skeleton

---
+++ Pachondi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SourceCode::Application.config.secret_token = 'a1991120a649192ee5564a33a8428c8d0a42bbf07a3d285a00621992323fde9896721c548ba008c0ba92eae460ad15f3df823b4f942a4b62e6b97014af1236dc'
---
+++ Pachondi/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SourceCode::Application.config.session_store :cookie_store, key: '_SourceCode_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SourceCode::Application.config.session_store :active_record_store
